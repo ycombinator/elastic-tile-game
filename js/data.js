56afaf37b8ccbc1e0b8b1a3055677cd8676e6d79
Shaunak Kashyap <ycombinator@gmail.com>
2015-07-18 18:29:03 -0700

    s/500/600/

--- js/data.js
+++ js/data.js
@@ -2,31 +2,31 @@ var data = {
 	"Elasticsearch": [
 		{ value: 200, answer: "A collection of similar documents" },
 		{ value: 400, answer: "A part of an index" },
-		{ value: 500, answer: "API that allows multiple operations to be performed at once" },
+		{ value: 600, answer: "API that allows multiple operations to be performed at once" },
 		{ value: 1000, answer: "Inter-node communication protocol" }
 	],
 	"Logstash": [
 		{ value: 200, answer: "Filter plugin to parse text using regular expression patterns" },
 		{ value: 400, answer: "Logstash implementation language" },
-		{ value: 500, answer: "Protocol used by Logstash Forwarder" },
+		{ value: 600, answer: "Protocol used by Logstash Forwarder" },
 		{ value: 1000, answer: "Type of plugin that can change the representation of an event" }
 	],
 	"Kibana": [
 		{ value: 200, answer: "" },
 		{ value: 400, answer: "" },
-		{ value: 500, answer: "" },
+		{ value: 600, answer: "" },
 		{ value: 1000, answer: "Allows custom formatting of fields" }
 	],
 	"Beats": [
 		{ value: 200, answer: "Provides realtime analytics for network protocols" },
 		{ value: 400, answer: "Library for building more beats" },
-		{ value: 500, answer: "" },
+		{ value: 600, answer: "" },
 		{ value: 1000, answer: "" }
 	],
 	"Commercial": [
 		{ value: 200, answer: "Elasticsearch as a service" },
 		{ value: 400, answer: "" },
-		{ value: 500, answer: "" },
+		{ value: 600, answer: "" },
 		{ value: 1000, answer: "" }
 	]
 }
