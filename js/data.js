39b523b17d67b8a9c3f1164270724132e9862d6b
Shaunak Kashyap <ycombinator@gmail.com>
2015-07-23 06:28:33 -0700

    Adding LS answers + untabify.

--- js/data.js
+++ js/data.js
@@ -1,108 +1,115 @@
 var data = {
-	"Elasticsearch": [
-		{ value: 200, answers: [
+  "Elasticsearch": [
+    { value: 200, answers: [
       "This is where you find this quote: \"You know, for Search\"",
       "This is the search library used internally by Elasticsearch",
-			"Port 9200",
-			"Port 9300",
-			"These are the two types of shards",
+      "Port 9200",
+      "Port 9300",
+      "These are the two types of shards",
       "This is the name of the founder of the Elasticsearch project"
-			]
-		},
-		{ value: 400, answers: [
+      ]
+    },
+    { value: 400, answers: [
       "An index is partitioned into multiple _____ (defaults to 5)",
-			"This is the default number of shards for an index",
-			"The API that allows multiple operations to be performed at once"
-			]
-		},
-		{ value: 600, answers: [
+      "This is the default number of shards for an index",
+      "The API that allows multiple operations to be performed at once"
+      ]
+    },
+    { value: 600, answers: [
       "This feature is commonly referred to as \"Search reversed\"",
       "This is one way to store geo data in Elasticsearch",
-			"This is the communication protocol used between nodes",
-			"This is the color of the cluster state when all your data is available but not replicated"
-			]
-		},
-		{ value: 1000, answers: [
+      "This is the communication protocol used between nodes",
+      "This is the color of the cluster state when all your data is available but not replicated"
+      ]
+    },
+    { value: 1000, answers: [
       "This is the default scoring algorithm used by Lucene",
       "This is the color of shirt Shay Banon wears every day"
-			]
-		}
-	],
-	"Logstash": [
-		{ value: 200, answers: [
-			"The name of the filter plugin to parse text using regular expression patterns",
-			"Input, Filter, Output, and Codec are various types of ______s",
+      ]
+    }
+  ],
+  "Logstash": [
+    { value: 200, answers: [
+      "The name of the filter plugin to parse text using regular expression patterns",
+      "Input, Filter, Output, and Codec are various types of ______s",
       "This is the name of the founder of the Logstash project"
-			]
-		},
-		{ value: 400, answers: [
-      "This is the programming language that Logstash is written in"
-			]
-		},
-		{ value: 600, answers: [
-		  "This is the name of the transport protocol used by Logstash Forwarder (Think Paul Bunyan)"
-			]
-		},
-    { value: 1000, answers: "This is the size of the input-to-filter queue" }
-	],
-	"Kibana": [
-		{ value: 200, answers: [
-			"This is the command to start the Kibana server on UNIX-like systems",
-			"Feature to automatically update the data shown in Kibana",
+      ]
+    },
+    { value: 400, answers: [
+      "This is the programming language that Logstash is written in",
+      "The name of the filter plugin that allows you to rename, replace, and modify fields in your events"
+      ]
+    },
+    { value: 600, answers: [
+      "This is the name of the transport protocol used by Logstash Forwarder (Think Paul Bunyan)",
+      "The name of the filter plugin that translates IP addresses in your data to geographical locations"
+      ]
+    },
+    { value: 1000, answers: [
+      "The name of the command line flag that tests the validity of your Logstash configuration file",
+      "The name of the command line flag that sets the number of filter worker threads",
+      "This is the minimum number of threads used by Logstash"
+      ]
+    }
+  ],
+  "Kibana": [
+    { value: 200, answers: [
+      "This is the command to start the Kibana server on UNIX-like systems",
+      "Feature to automatically update the data shown in Kibana",
       "This is the name of the founder of the Kibana project"
-			]
-		},
-		{ value: 400, answers: "This is the default time span in the Discover view" },
-		{ value: 600, answers: [
-			"This is the special Elasticsearch index where Kibana stores its data",
-			"This is the expression language used for searching in Kibana"
-			]
-		},
-		{ value: 1000, answers: [
-			"This is the programming language in which the first version of Kibana was written",
-			"This feature allows you to customize the display format of a field"
-		]
-		}
-	],
-	"Beats": [
-		{ value: 200, answers: [
+      ]
+    },
+    { value: 400, answers: "This is the default time span in the Discover view" },
+    { value: 600, answers: [
+      "This is the special Elasticsearch index where Kibana stores its data",
+      "This is the expression language used for searching in Kibana"
+      ]
+    },
+    { value: 1000, answers: [
+      "This is the programming language in which the first version of Kibana was written",
+      "This feature allows you to customize the display format of a field"
+    ]
+    }
+  ],
+  "Beats": [
+    { value: 200, answers: [
       "This beat provides realtime analytics for network protocols",
       "This is the programming language in which Packetbeat is written"
       ]
     },
-		{ value: 400, answers: [
+    { value: 400, answers: [
       "The name of the platform for building more beats",
       "The name of the beat that is based on Logstash Forwarder",
       "The name of the beat that tracks CPU and memory statistics per process"
       ]
     },
-		{ value: 600, answers: [
+    { value: 600, answers: [
       "This is the configuration file format used by Packetbeat",
       "The name of the field used by Packetbeat to store the HTTP status code"
       ]
     },
-		{ value: 1000, answers: [
+    { value: 1000, answers: [
       "This is the first database protocol supported by Packetbeat",
       "The name of the Go library used by Packetbeat to sniff packets"
       ]
     }
-	],
-	"Commercial": [
-		{ value: 200, answers: [
-			"This plugin enables password protection for Elasticsearch",
-			"This product offers Elasticsearch as a Service"
-			]
-		},
-		{ value: 400, answers: [
-			"Development, Silver, Gold, and Platinum are ________ plans offered by Elastic.co",
-			"The free trial period for Found is ____ days"
-			]
-		},
-		{ value: 600, answers: [
-			"This plugin enables alerting and notifications on Elasticsearch data",
-			"The starting rate for a Found cluster is $____/month"
-			]
-		},
-		{ value: 1000, answers: "This subscription level does not include phone support" }
-	]
+  ],
+  "Commercial": [
+    { value: 200, answers: [
+      "This plugin enables password protection for Elasticsearch",
+      "This product offers Elasticsearch as a Service"
+      ]
+    },
+    { value: 400, answers: [
+      "Development, Silver, Gold, and Platinum are ________ plans offered by Elastic.co",
+      "The free trial period for Found is ____ days"
+      ]
+    },
+    { value: 600, answers: [
+      "This plugin enables alerting and notifications on Elasticsearch data",
+      "The starting rate for a Found cluster is $____/month"
+      ]
+    },
+    { value: 1000, answers: "This subscription level does not include phone support" }
+  ]
 }
