56997734713da2ac35333f3f89a7c5dfb9497338
Shaunak Kashyap <ycombinator@gmail.com>
2015-07-22 17:29:40 -0700

    Improving the wording for answers.

--- js/data.js
+++ js/data.js
@@ -1,106 +1,108 @@
 var data = {
 	"Elasticsearch": [
 		{ value: 200, answers: [
-			"A collection of similar documents",
-			"Search library used internally by Elasticsearch",
-			"9200",
-			"9300",
-			"The two types of shards"
+      "This is where you find this quote: \"You know, for Search\"",
+      "This is the search library used internally by Elasticsearch",
+			"Port 9200",
+			"Port 9300",
+			"These are the two types of shards",
+      "This is the name of the founder of the Elasticsearch project"
 			]
 		},
 		{ value: 400, answers: [
-		  "An index is made of multiple ______",
-			"Default number of shards for an index",
-			"API that allows multiple operations to be performed at once"
+      "An index is partitioned into multiple _____ (defaults to 5)",
+			"This is the default number of shards for an index",
+			"The API that allows multiple operations to be performed at once"
 			]
 		},
 		{ value: 600, answers: [
-      "Search reversed",
-			"Latest released version of Elasticsearch",
-			"Inter-node communication protocol",
-			"All primary shards active but not all replica shards active"
+      "This feature is commonly referred to as \"Search reversed\"",
+      "This is one way to store geo data in Elasticsearch",
+			"This is the communication protocol used between nodes",
+			"This is the color of the cluster state when all your data is available but not replicated"
 			]
 		},
 		{ value: 1000, answers: [
-			"ceil( Number of replica shards / 2 ) + 1",
-			"Finite State Transducer (FST)"
+      "This is the default scoring algorithm used by Lucene",
+      "This is the color of shirt Shay Banon wears every day"
 			]
 		}
 	],
 	"Logstash": [
 		{ value: 200, answers: [
-			"Filter plugin to parse text using regular expression patterns",
-			"Input, Filter, Output, and Codec"
+			"The name of the filter plugin to parse text using regular expression patterns",
+			"Input, Filter, Output, and Codec are various types of ______s",
+      "This is the name of the founder of the Logstash project"
 			]
 		},
 		{ value: 400, answers: [
-			"Programming language in which Logstash is written"
+      "This is the programming language that Logstash is written in"
 			]
 		},
 		{ value: 600, answers: [
-		  "Protocol used by Logstash Forwarder"
+		  "This is the name of the transport protocol used by Logstash Forwarder (Think Paul Bunyan)"
 			]
 		},
-    { value: 1000, answers: "Size of the input -> filter queue" }
+    { value: 1000, answers: "This is the size of the input-to-filter queue" }
 	],
 	"Kibana": [
 		{ value: 200, answers: [
-			"Command to start the Kibana server on UNIX-like systems",
-			"Feature to automatically update the data shown in Kibana"
+			"This is the command to start the Kibana server on UNIX-like systems",
+			"Feature to automatically update the data shown in Kibana",
+      "This is the name of the founder of the Kibana project"
 			]
 		},
-		{ value: 400, answers: "The default time span in the Discover view" },
+		{ value: 400, answers: "This is the default time span in the Discover view" },
 		{ value: 600, answers: [
-			"Special Elasticsearch index where Kibana stores its data",
-			"The expression language used for searching",
-			"Allows custom formatting of fields"
+			"This is the special Elasticsearch index where Kibana stores its data",
+			"This is the expression language used for searching in Kibana"
 			]
 		},
 		{ value: 1000, answers: [
-			"Programming language in which the first version of Kibana was written",
-			"Library used by the number formatting template"
-			]
+			"This is the programming language in which the first version of Kibana was written",
+			"This feature allows you to customize the display format of a field"
+		]
 		}
 	],
 	"Beats": [
 		{ value: 200, answers: [
-      "Provides realtime analytics for network protocols",
-      "Programming language in which Packetbeat is written"
+      "This beat provides realtime analytics for network protocols",
+      "This is the programming language in which Packetbeat is written"
       ]
     },
 		{ value: 400, answers: [
-      "Platform for building more beats",
-      "Beat based on Logstash Forwarder",
-      "Beat that tracks CPU and memory statistics per process"
+      "The name of the platform for building more beats",
+      "The name of the beat that is based on Logstash Forwarder",
+      "The name of the beat that tracks CPU and memory statistics per process"
       ]
     },
 		{ value: 600, answers: [
-      "Configuration file format used by Packetbeat",
-      "Packetbeat stores the HTTP status code in this field"
+      "This is the configuration file format used by Packetbeat",
+      "The name of the field used by Packetbeat to store the HTTP status code"
       ]
     },
 		{ value: 1000, answers: [
-      "First database protocol supported by Packetbeat",
-      "Go library used by Packetbeat to sniff packets"
+      "This is the first database protocol supported by Packetbeat",
+      "The name of the Go library used by Packetbeat to sniff packets"
       ]
     }
 	],
 	"Commercial": [
 		{ value: 200, answers: [
-			"Password protection for Elasticsearch",
-			"Elasticsearch as a service"
+			"This plugin enables password protection for Elasticsearch",
+			"This product offers Elasticsearch as a Service"
 			]
 		},
 		{ value: 400, answers: [
-			"Development, Silver, Gold, and Platinum",
-			"Free trial period for Found"
+			"Development, Silver, Gold, and Platinum are ________ plans offered by Elastic.co",
+			"The free trial period for Found is ____ days"
 			]
 		},
 		{ value: 600, answers: [
-			"Alerting for Elasticsearch",
-			"Starting rate for a Found cluster"
+			"This plugin enables for alerting and notifications on Elasticsearch data",
+			"The starting rate for a Found cluster is $____/month"
 			]
 		},
-		{ value: 1000, answers: "Subscription level not offering phone support" }
+		{ value: 1000, answers: "This subscription level does not include phone support" }
 	]
 }
